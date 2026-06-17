aa8208382fcb5376d994986217abd76ea58157bf
ST10398576 <ST10398576@rcconnect.edu.za>
2026-06-17 23:50:11 +0200

    Updating Testing

--- ST10398576_TechMoveGLMS.Tests/IntegrationTests/IntegrationTestBase.cs
+++ ST10398576_TechMoveGLMS.Tests/IntegrationTests/IntegrationTestBase.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication;
 using ST10398576_TechMoveGLMS.API;
 using ST10398576_TechMoveGLMS.DBContext;
 
@@ -15,6 +16,13 @@ public class IntegrationTestBase : IClassFixture<WebApplicationFactory<ST1039857
             builder.UseSetting("environment", "Testing");
             builder.ConfigureServices(services =>
             {
+                    // Add a test authentication scheme that automatically authenticates requests for integration tests
+                    services.AddAuthentication(options =>
+                    {
+                        options.DefaultAuthenticateScheme = "Test";
+                        options.DefaultChallengeScheme = "Test";
+                    }).AddScheme<AuthenticationSchemeOptions, TestAuthHandler>("Test", options => { });
+
                     // Replace DBContext registrations with InMemory for testing
                     var descriptors = services.Where(d =>
                         d.ServiceType == typeof(DbContextOptions<TechMoveDBContext>) ||
